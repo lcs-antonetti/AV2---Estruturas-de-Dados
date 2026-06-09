d776b18943a3bd9e743e27de2491275ef6d73c4d
Gabriel Sena <85597607+gsenaDevHub@users.noreply.github.com>
2026-06-09 17:56:20 -0300

    Add files via upload

--- Pacientes/paciente.cpp
+++ Pacientes/paciente.cpp
@@ -3,19 +3,50 @@
 
 using namespace std;
 
-cadastro(Paciente &p){
+int cadastrar(PacienteLista &l){
+    Paciente* novo = new Paciente;
+
+    cin >> novo->id;
+    cin.ignore();              
+    getline(cin, novo->nome);
+    cin >> novo->idade;
+    cin.ignore();             
+    getline(cin, novo->cpf);
+    getline(cin, novo->telefone);
+    getline(cin, novo->endereco);
+    getline(cin, novo->sintomas);
+
+    novo->prox = nullptr;
+
+    if(l.inicio == nullptr){
+        l.inicio = novo;
+        return 1;
+    }
+
+    Paciente* aux = l.inicio;
+
+    while(aux->prox != nullptr){
+        aux = aux->prox;
+    }
+
+    aux->prox = novo;
+
+    return 0;
+}
+int remover(PacienteLista &l){
 	
 }
-remover(Paciente &p){
+void lista(PacienteLista &l){
+	Paciente* aux = l.inicio;
+    while(aux != nullptr){
+        cout << aux->nome << endl;
+        aux = aux->prox
+    }
+}
+void buscar(PacienteLista &l){
 	
 }
-lista(){
-	
-}
-buscar(){
-	
-}
-atualizar(Paciente &p){
+int atualizar(Paciente &p, PacienteLista &l){
 	
 }
 
